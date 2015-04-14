4814dcfea472445e5826a6e66cb08821e23d5880
lizmorr <efmorr@gmail.com>
2015-04-14 12:58:47 -0400

    Fix style errors

--- spec/models/carbon_source_spec.rb
+++ spec/models/carbon_source_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 describe CarbonSource do
   it { should validate_presence_of(:source) }
   it { should have_valid(:source).when("Heating oil", "Electric") }
-  it { should_not have_valid(:source).when("",nil) }
+  it { should_not have_valid(:source).when("", nil) }
 
   it { should validate_presence_of(:conversion_factor) }
-  it { should have_valid(:conversion_factor).when(0.023,1) }
+  it { should have_valid(:conversion_factor).when(0.023, 1) }
   it { should_not have_valid(:conversion_factor).when(nil, "small") }
 
   it { should validate_presence_of(:conversion_units) }
