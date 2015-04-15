f3c7ef4e5834603d775b82183595b8a20daab512
lizmorr <efmorr@gmail.com>
2015-04-15 10:52:02 -0400

    Add index page for view to see all carbon sources. Passes test.

--- spec/features/carbon_sources/user_views_all_sources_spec.rb
+++ spec/features/carbon_sources/user_views_all_sources_spec.rb
@@ -14,19 +14,17 @@ feature 'user views all potential carbon sources', %{
     home_source1 = FactoryGirl.create(:carbon_source,
       source: 'Electricity',
       category: 'Home')
-
     home_source2 = FactoryGirl.create(:carbon_source,
       source: 'Natural gas',
       category: 'Home')
-
     home_source3 = FactoryGirl.create(:carbon_source,
       source: 'Heating oil',
       category: 'Home')
 
     visit carbon_sources_path
 
-    within('.home_index') do
-      expect(page).to have_content('Electricty')
+    within('#Home_index') do
+      expect(page).to have_content('Electricity')
       expect(page).to have_content('Heating oil')
       expect(page).to have_content('Natural gas')
     end
@@ -34,7 +32,7 @@ feature 'user views all potential carbon sources', %{
     expect(page.body.index('Electricity') < page.body.index('Heating oil'))
     expect(page.body.index('Heating oil') < page.body.index('Natural gas'))
 
-    within('.travel_index') do
+    within('#Travel_index') do
       expect(page).to have_content('Gasoline')
     end
   end
