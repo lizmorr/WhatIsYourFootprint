6c2af5ea11514e109349c27982848fa5a59830ed
lizmorr <efmorr@gmail.com>
2015-04-15 13:24:32 -0400

    Get rid of show view for carbon sources

--- spec/features/carbon_sources/user_views_all_sources_spec.rb
+++ spec/features/carbon_sources/user_views_all_sources_spec.rb
@@ -11,13 +11,23 @@ feature 'user views all potential carbon sources', %{
 
     FactoryGirl.create(:carbon_source, source: 'Electricity', category: 'Home')
     FactoryGirl.create(:carbon_source, source: 'Natural gas', category: 'Home')
-    FactoryGirl.create(:carbon_source, source: 'Heating oil', category: 'Home')
+    detail_source = FactoryGirl.create(:carbon_source,
+      source: 'Heating oil',
+      conversion_factor: 0.01213,
+      conversion_units: 'tons CO2/gallon',
+      category: 'Home',
+      subcategory: 'Heating'
+)
 
     visit carbon_sources_path
 
     within('#Home_index') do
       expect(page).to have_content('Electricity')
       expect(page).to have_content('Heating oil')
+      within("#source_#{detail_source.id}") do
+        expect(page).to have_content('Conversion: 0.01213 tons CO2/gallon')
+        expect(page).to have_content('Subcategory: Heating')
+      end
       expect(page).to have_content('Natural gas')
     end
 
