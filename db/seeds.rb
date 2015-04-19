83f27a72613352911fd770497df043f564c0f2c5
lizmorr <efmorr@gmail.com>
2015-04-19 16:41:59 -0400

    Correct seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,14 @@
-CarbonUse.create(
+CarbonSource.create(
   name: "Fuel Oil",
   conversion_factor: 22.61,
   conversion_units: "lbs / gallon"
 )
-CarbonUse.create(
+CarbonSource.create(
   name: "Natural Gas",
   conversion_factor: 0.12,
   conversion_units: "lbs / cubic foot"
 )
-CarbonUse.create(
+CarbonSource.create(
   name: "Fuel Oil",
   conversion_factor: 22.61,
   conversion_units: "lbs / gallon"
