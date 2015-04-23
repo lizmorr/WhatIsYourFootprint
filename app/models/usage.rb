e701b15fffc18eb2aa343803d65c0a22770d3db9
Liz Morrison <lizmorr@users.noreply.github.com>
2015-04-22 20:26:34 -0400

    Update usage.rb

--- app/models/usage.rb
+++ app/models/usage.rb
@@ -80,7 +80,7 @@ class Usage < ActiveRecord::Base
   def self.daily_emissions_summary(user, start_date, end_date)
     start_date = Date.parse(start_date.to_s)
     end_date = Date.parse(end_date.to_s)
-    daily_emissions_hash = {"Type" => "Daily Emissions", "Data" => [] }
+    daily_emissions_hash = { "Type" => "Daily Emissions", "Data" => [] }
     start_date.step(end_date).each do |date|
       daily_emissions_hash["Data"] << {
         "Date" => date.strftime("%m/%d/%Y"),
