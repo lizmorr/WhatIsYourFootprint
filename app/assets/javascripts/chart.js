1d480fd43c98443583a1e55f9409d3011f0e570f
lizmorr <efmorr@gmail.com>
2015-04-24 13:33:36 -0400

    Correct hound style issues

--- app/assets/javascripts/chart.js
+++ app/assets/javascripts/chart.js
@@ -5,17 +5,17 @@ $(function() {
         var data = emissionsSummary.Data;
 
         var margin = {top: 10, right: 40, bottom: 40, left:40},
-            width = 600,
-            height = 500;
+            w = 600,
+            h = 500;
 
         var x = d3.time.scale()
             .domain([new Date(data[0].Date),
                 d3.time.day.offset(new Date(data[data.length - 1].Date), 1)])
-            .rangeRound([0, width - margin.left - margin.right]);
+            .rangeRound([0, w - margin.left - margin.right]);
 
         var y = d3.scale.linear()
             .domain([0, d3.max(data, function(d) { return d.Value; })])
-            .range([height - margin.top - margin.bottom, 0]);
+            .range([h - margin.top - margin.bottom, 0]);
 
         var xAxis = d3.svg.axis()
             .scale(x)
@@ -30,8 +30,8 @@ $(function() {
 
         var svg = d3.select('#chart').append('svg')
             .attr('class', 'chart')
-            .attr('width', width)
-            .attr('height', height)
+            .attr('width', w)
+            .attr('height', h)
           .append('g')
             .attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')');
 
@@ -39,14 +39,20 @@ $(function() {
             .data(data)
           .enter().append('rect')
             .attr('class', 'bar')
-            .attr('x', function(d) { return x(new Date(d.Date)); })
-            .attr('y', function(d) { return height - margin.top - margin.bottom - (height - margin.top - margin.bottom - y(d.Value)) })
+            .attr('x', function(d) {
+                return x(new Date(d.Date));
+            })
+            .attr('y', function(d) {
+                return h - margin.top - margin.bottom - (h - margin.top - margin.bottom - y(d.Value));
+            })
             .attr('width', 10)
-            .attr('height', function(d) { return height - margin.top - margin.bottom - y(d.Value) });
+            .attr('height', function(d) {
+                return h - margin.top - margin.bottom - y(d.Value);
+            });
 
         svg.append('g')
             .attr('class', 'x axis')
-            .attr('transform', 'translate(0, ' + (height - margin.top - margin.bottom) + ')')
+            .attr('transform', 'translate(0, ' + (h - margin.top - margin.bottom) + ')')
             .call(xAxis);
 
         svg.append('g')
