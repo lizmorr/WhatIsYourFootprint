3e9e4d5552471d16aaf9d8a98845d9a87ca5b737
lizmorr <efmorr@gmail.com>
2015-04-24 12:18:09 -0400

    Daily emissions chart properly displayed

--- app/assets/javascripts/chart.js
+++ app/assets/javascripts/chart.js
@@ -1,94 +1,56 @@
 $(function() {
 
-  var emissionsSummary = "http://localhost:3000/usages.json";
-  $.getJSON(emissionsSummary, function (emissionsSummary) {
+    var emissionsSummary = "http://localhost:3000/usages.json";
+    $.getJSON(emissionsSummary, function (emissionsSummary) {
+        var data = emissionsSummary.Data;
 
-    var margin = {
-      top: 0,
-      right: 0,
-      bottom: 60,
-      left: 0
-    };
+        var margin = {top: 10, right: 40, bottom: 40, left:40},
+            width = 600,
+            height = 500;
 
-    var w = 600 - margin.left - margin.right;
-    var h = 250 - margin.top - margin.bottom;
-    var padding = 40;
+        var x = d3.time.scale()
+            .domain([new Date(data[0].Date), d3.time.day.offset(new Date(data[data.length - 1].Date), 1)])
+            .rangeRound([0, width - margin.left - margin.right]);
 
-    var dataset = emissionsSummary.Data;
+        var y = d3.scale.linear()
+            .domain([0, d3.max(data, function(d) { return d.Value; })])
+            .range([height - margin.top - margin.bottom, 0]);
 
-    var printDate = d3.time.format("%m/%d");
+        var xAxis = d3.svg.axis()
+            .scale(x)
+            .orient('bottom')
+            .ticks(d3.time.weeks)
+            .tickPadding(8);
 
-    var xScale = d3.time.scale()
-                   .domain([new Date(dataset[0].Date), d3.time.day.offset(new Date(dataset[dataset.length-1].Date),1)])
-                   .rangeRound([0, w - margin.left - margin.right]);
+        var yAxis = d3.svg.axis()
+            .scale(y)
+            .orient('left')
+            .tickPadding(8);
 
-    var yScale = d3.scale.linear()
-                   .domain([0, d3.max(dataset, function(d) {
-                     return d.Value;
-                   })])
-                   .range([h - padding, padding]);
+        var svg = d3.select('#chart').append('svg')
+            .attr('class', 'chart')
+            .attr('width', width)
+            .attr('height', height)
+          .append('g')
+            .attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')');
 
-    var key = function(d) {
-      return d.Date;
-    };
+        svg.selectAll('.chart')
+            .data(data)
+          .enter().append('rect')
+            .attr('class', 'bar')
+            .attr('x', function(d) { return x(new Date(d.Date)); })
+            .attr('y', function(d) { return height - margin.top - margin.bottom - (height - margin.top - margin.bottom - y(d.Value)) })
+            .attr('width', 10)
+            .attr('height', function(d) { return height - margin.top - margin.bottom - y(d.Value) });
 
-    var svg = d3.select("#chart")
-        .append("svg")
-        .attr("width", w + margin.left + margin.right)
-        .attr("height", h + margin.top + margin.bottom )
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        svg.append('g')
+            .attr('class', 'x axis')
+            .attr('transform', 'translate(0, ' + (height - margin.top - margin.bottom) + ')')
+            .call(xAxis);
 
-    var xAxis = d3.svg.axis()
-        .scale(xScale)
-        .orient("bottom")
-        .ticks(d3.time.sunday);
+        svg.append('g')
+          .attr('class', 'y axis')
+          .call(yAxis);
 
-    var yAxis = d3.svg.axis()
-        .scale(xScale)
-        .orient("left")
-        .tickPadding(8);
-
-     svg.selectAll("rect")
-        .data(dataset)
-        .enter()
-        .append("rect")
-        .attr("x", function (d) {
-          return xScale(new Date(d.Date));
-        })
-        .attr("y", function(d) {
-          return h - yScale(d.Value);
-        })
-        .attr("width", (w / dataset.length))
-        .attr("height", function(d) {
-          return yScale(d.Value);
-        })
-        .attr("fill", "teal");
-
-     svg.append("g")
-        .attr("class", "axis")
-        .attr("transform", "translate(0," + (h + 5) + ")")
-        .call(xAxis);
-     svg.append("g")
-        .attr("class", "axis")
-        .call(yAxis);
-
-
-     svg.selectAll("text")
-        .data(dataset)
-        .enter()
-        .append("text")
-        .text(function(d) {
-          return d.Value;
-        })
-        .attr("text-anchor", "middle")
-        .attr("x", function (d, i) {
-          return xScale(i) + (w / dataset.length) / 2;
-        })
-        .attr("y", function (d) {
-          return h - yScale(d.Value) + 14;
-        })
-        .attr("font-family", "sans-serif")
-        .attr("font-size", "11px")
-        .attr("fill", "white");
   });
 });
