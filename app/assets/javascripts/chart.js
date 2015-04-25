2125a6fc25ff2379db88e5948592636429cddbef
lizmorr <efmorr@gmail.com>
2015-04-25 18:28:22 -0400

    Add title to chart

--- app/assets/javascripts/chart.js
+++ app/assets/javascripts/chart.js
@@ -17,15 +17,35 @@ $(function() {
       function draw(emissionsSummary) {
           var data = emissionsSummary.Data;
 
-          var margin = {top: 10, right: 40, bottom: 40, left:40},
+          var margin = {top: 40, right: 30, bottom: 10, left: 50},
               w = 600,
               h = 500;
 
-          var min = d3.min(data, function (d) {
+          var printDate = d3.time.format("%m/%d");
+          var printMonth = d3.time.format("%B");
+
+          var barPadding = 1;
+
+          var minDate = d3.min(data, function(d) {
             return d.Date;
           });
 
-          console.log(min);
+          var maxDate = d3.max(data, function(d) {
+            return d.Date;
+          });
+
+          console.log(minDate + " - " + maxDate);
+
+          if (data.length <= 15) {
+            var dateTicksType = d3.time.days;
+            var dateLabelFormat = printDate;
+          } else if (data.length <= 90) {
+            var dateTicksType = d3.time.weeks;
+            var dateLabelFormat = printDate;
+          } else {
+            var dateTicksType = d3.time.months;
+            var dateLabelFormat = printMonth;
+          }
 
           var x = d3.time.scale()
               .domain([new Date(data[0].Date),
@@ -39,7 +59,8 @@ $(function() {
           var xAxis = d3.svg.axis()
               .scale(x)
               .orient('bottom')
-              .ticks(d3.time.weeks)
+              .ticks(dateTicksType)
+              .tickFormat(dateLabelFormat)
               .tickPadding(8);
 
           var yAxis = d3.svg.axis()
@@ -64,19 +85,29 @@ $(function() {
               .attr('y', function(d) {
                   return h - margin.top - margin.bottom - (h - margin.top - margin.bottom - y(d.Value));
               })
-              .attr('width', 10)
+              .attr('width', function(d) {
+                return ((w - margin.left - margin.right) / data.length) - barPadding;
+              })
               .attr('height', function(d) {
                   return h - margin.top - margin.bottom - y(d.Value);
               });
 
           svg.append('g')
-              .attr('class', 'x axis')
-              .attr('transform', 'translate(0, ' + (h - margin.top - margin.bottom) + ')')
-              .call(xAxis);
+             .attr('class', 'x axis')
+             .attr('transform', 'translate(0, ' + (h - margin.top - margin.bottom) + ')')
+             .call(xAxis);
 
           svg.append('g')
-            .attr('class', 'y axis')
-            .call(yAxis);
+             .attr('class', 'y axis')
+             .call(yAxis);
+
+          svg.append("text")
+             .attr("class", "title")
+             .attr("x", (w / 2))
+             .attr("y", 0 - (margin.top / 2))
+             .attr("text-anchor", "middle")
+             .text("Daily Emissions from " + minDate + " - " + maxDate);
+
     };
   }
 });
