0c6ea2db9b49a88070833b2b9d0d5bad96a15ef5
lizmorr <efmorr@gmail.com>
2015-04-28 17:38:36 -0400

    Update seed file to be re-runnable. Eventually move to CSV.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,32 +1,32 @@
-CarbonSource.create(
+CarbonSource.find_or_create_by(
   name: "Fuel Oil",
   conversion_factor: 22.61,
   units: "gallons",
   citation: "Environmental Protection Agency (EPA)",
   citation_url: "http://epa.gov/climatechange/ghgemissions/ind-assumptions.html"
 )
-CarbonSource.create(
+CarbonSource.find_or_create_by(
   name: "Natural Gas",
   conversion_factor: 0.12,
   units: "therms",
   citation: "Environmental Protection Agency (EPA)",
   citation_url: "http://epa.gov/climatechange/ghgemissions/ind-assumptions.html"
 )
-CarbonSource.create(
+CarbonSource.find_or_create_by(
   name: "Propane",
   conversion_factor: 12.43,
   units: "gallons",
   citation: "Environmental Protection Agency (EPA)",
   citation_url: "http://epa.gov/climatechange/ghgemissions/ind-assumptions.html"
 )
-CarbonSource.create(
+CarbonSource.find_or_create_by(
   name: "Electricity",
   conversion_factor: 1.13,
   units: "kWhs",
   citation: "Blue Sky",
   citation_url: "http://blueskymodel.org/kilowatt-hour"
 )
-CarbonSource.create(
+CarbonSource.find_or_create_by(
   name: "Unleaded Gas",
   conversion_factor: 19.64,
   units: "gallons",
@@ -34,6 +34,6 @@ CarbonSource.create(
   citation_url: "http://www.eia.gov/tools/faqs/faq.cfm?id=307&t=11"
 )
 
-UseReason.create(name: "Home")
-UseReason.create(name: "Travel")
-UseReason.create(name: "Work")
+UseReason.find_or_create_by(name: "Home")
+UseReason.find_or_create_by(name: "Travel")
+UseReason.find_or_create_by(name: "Work")
